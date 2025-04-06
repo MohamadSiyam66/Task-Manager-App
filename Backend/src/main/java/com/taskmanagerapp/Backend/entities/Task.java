023630341124cd5896c9ba5364252db4697b9e79
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2025-04-06 23:27:07 +0530

    chore: updated entity with relationships

--- Backend/src/main/java/com/taskmanagerapp/Backend/entities/Task.java
+++ Backend/src/main/java/com/taskmanagerapp/Backend/entities/Task.java
@@ -1,8 +1,13 @@
 package com.taskmanagerapp.Backend.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.taskmanagerapp.Backend.dto.TaskDto;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,6 +22,32 @@ public class Task {
     private String title;
     private String description;
     private String status;
+
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now(); // Automatically set current timestamp
+        }
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private User user;
+
+    public TaskDto getTaskDto(){
+        TaskDto taskDto = new TaskDto();
+        taskDto.setId(id);
+        taskDto.setTitle(title);
+        taskDto.setDescription(description);
+        taskDto.setStatus(status);
+        taskDto.setUserName(user.getUsername());
+        taskDto.setUserId(user.getId());
+        return taskDto;
+    }
 
 }
